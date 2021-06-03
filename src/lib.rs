c33b3599f00a516df90a9ef85da497f5127a6a41
Luke <luke.wilde@live.co.uk>
2021-06-03 21:55:44 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,113 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+use std::fmt;
+use js_sys::Math::random;
+use fixedbitset::FixedBitSet;
+
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: FixedBitSet,
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbour_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width -1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbour_row = (row + delta_row) % self.height;
+                let neighbour_col = (column + delta_col) % self.width;
+                let index = self.get_index(neighbour_row, neighbour_col);
+                count += self.cells[index] as u8;
+            }
+        }
+
+        count
+    }
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let index = self.get_index(row, col);
+                let cell = self.cells[index];
+                let live_neighbours = self.live_neighbour_count(row, col);
+
+                next.set(index, match (cell, live_neighbours) {
+                    (true, x) if x < 2 => false,
+                    (true, 2) | (true, 3) => true,
+                    (true, x) if x > 3 => false,
+                    (false, 3) => true,
+                    (otherwise, _) => otherwise,
+                });
+            }
+        }
+
+        self.cells = next;
+    }
+
+    pub fn new() -> Universe {
+        utils::set_panic_hook();
+        let width = 64;
+        let height = 64;
+
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            cells.set(i, js_sys::Math::random() < 0.5)
+        }
+
+        Universe {
+            width,
+            height,
+            cells
+        }
+    }
+
+    pub fn toggle_cell(&mut self, row: u32, column: u32) {
+        let index = self.get_index(row, column);
+        self.cells.set(index, !self.cells[index]);
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
+    }
+}
